09d6d05d830dbf17d9b9469d2673a928333e50c3
shouta totsuka <5h0utat0t2uka@gmail.com>
2023-12-27 06:56:06 +0900

    Add CORS support and rate limit warning UI

--- src/app/api/demo/route.ts
+++ src/app/api/demo/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server';
 
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': 'https://next-middleware-demo.vercel.app, http://localhost:3000',
+  'Access-Control-Allow-Methods': 'GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
+
 export async function GET(request: Request) {
-  return NextResponse.json({
-    message: 'Hello from /api/demo'
-  })
+  return NextResponse.json(
+    { message: 'Hello from `/api/demo`' },
+    { status: 200, headers: corsHeaders }
+  )
 }
